6214f401e9f2bf117fa79127fd33b4cf4318d5d0
tenshir00 <96345827+tenshir00@users.noreply.github.com>
2025-07-07 12:21:04 -0500

    Fix tower image cropping to show top priority

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ useEffect(() => {
         <img 
           src="/tower.jpg" 
           alt="UT Austin Tower" 
-          className="block lg:hidden w-full h-full object-cover object-center grayscale group-hover:grayscale-0 transition-all duration-500 group-hover:scale-105"
+          className="block lg:hidden w-full h-full object-cover object-top grayscale group-hover:grayscale-0 transition-all duration-500 group-hover:scale-105"
         />
       </div>
     </div>
