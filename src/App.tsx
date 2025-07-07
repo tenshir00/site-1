45beab74afb59f503312b05b04bdeabe9b6d1aa0
tenshir00 <96345827+tenshir00@users.noreply.github.com>
2025-07-07 11:42:34 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import About from './components/About';
